400bdd32a92e3641674428c5528f87f95c3fe995
rhodinemma <63339234+rhodinemma@users.noreply.github.com>
2022-09-11 15:47:51 +0300

    create database models

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
 const mongoose = require("./database/mongoose");
+
+const TaskList = require("./database/models/taskList");
+const Task = require("./database/models/Task");
+
+app.use(cors());
+app.use(express.json());
 
 app.listen(5000, () => {
   console.log("Server started on port 5000");
