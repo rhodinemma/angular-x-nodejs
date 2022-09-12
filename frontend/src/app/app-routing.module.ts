26341012be006095316b9aa19ca89dd7b1cd47da
rhodinemma <63339234+rhodinemma@users.noreply.github.com>
2022-09-12 09:50:48 +0300

    create placeholders for tasklist and tasks

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TaskScreenComponent } from './screens/task-screen/task-screen.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TaskScreenComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/screens/task-screen/task-screen.component.ts
+++ frontend/src/app/screens/task-screen/task-screen.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaskScreenComponent implements OnInit {
 
+  taskLists: any[] = [];
+  tasks: any[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
