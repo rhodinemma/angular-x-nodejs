0e471b3cc32cc1332d609bfc47235c9c6af712c7
rhodinemma <63339234+rhodinemma@users.noreply.github.com>
2022-09-12 10:19:15 +0300

    implementing services for api calls

--- frontend/src/app/api-config.service.ts
+++ frontend/src/app/api-config.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiConfigService {
 
-  constructor() { }
+  BASE_URL = 'http://localhost:5000';
+
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  // get API call
+  get(url: string){
+    return this.httpClient.get(`${this.BASE_URL}/${url}`);
+  }
+
+  // post API call
+  post(url: string, data: Object){
+    return this.httpClient.post(`${this.BASE_URL}/${url}`, data);
+  }
+
+  // put API call
+  put(url: string, data: Object){
+    return this.httpClient.put(`${this.BASE_URL}/${url}`, data);
+  }
+
+  delete(url: string){
+    return this.httpClient.delete(`${this.BASE_URL}/${url}`);
+  }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TaskScreenComponent } from './screens/task-screen/task-screen.component';
@@ -12,7 +12,8 @@ import { TaskScreenComponent } from './screens/task-screen/task-screen.component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
