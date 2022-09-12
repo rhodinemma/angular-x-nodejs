b2be023529ba1307ca4d50413b66aac947be3240
rhodinemma <63339234+rhodinemma@users.noreply.github.com>
2022-09-12 15:20:28 +0300

    write functions to get and createTaskList, get and createTask

--- frontend/src/app/task.service.ts
+++ frontend/src/app/task.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { ApiConfigService } from './api-config.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
 
-  constructor() { }
+  constructor(private apiConfigService: ApiConfigService) { }
+
+  // fetch all task lists
+  getAllTaskLists(){
+    return this.apiConfigService.get('tasklists');
+  }
+
+  // create a task list bucket
+  createTaskList(title: string){
+    let data = { 'title': title};
+    return this.apiConfigService.post('tasklists', data);
+  }
+
+  // fetch all tasks inside a tasklist object
+  getAlltasksForARaskList(taskListId:string){
+    return this.apiConfigService.get(`tasklists/${taskListId}/tasks`);
+  }
+
+  // create a task inside a particular tasklist object
+  createTaskInsideATaskList(taskListId: string, title: string){
+    let data = { 'title': title}
+    this.apiConfigService.post(`tasklists/${taskListId}/tasks`, data);
+  }
+
 }
